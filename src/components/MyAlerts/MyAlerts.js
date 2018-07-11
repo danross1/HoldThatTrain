f9e15d34c5a6c34e08709405f70eaa30a6de0b29
Dan Ross <dr.ross15@gmail.com>
2018-07-10 19:28:37 -0500

    created my alerts mock-up

--- src/components/MyAlerts/MyAlerts.js
+++ src/components/MyAlerts/MyAlerts.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Button from '@material-ui/core/Button';
 
 import Nav from '../../components/Nav/Nav';
 
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
 
+// const styles = {
+//   title: {
+//     fontSize: 14
+//   }
+// }
 
 const mapStateToProps = state => ({
   user: state.user,
@@ -38,6 +49,35 @@ class MyAlerts extends Component {
           >
             Welcome, { this.props.user.userName }!
           </h1>
+
+          <Card>
+            <CardHeader title="Dan's Morning Commute" />
+            <CardContent>
+              <div>Prospect Park</div>
+              <div>East</div>
+              <div>5 min before</div>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained" size="small">Delete</Button>
+              <Button variant="contained" size="small">Edit</Button>
+              <Button variant="contained" size="small">Activate</Button>
+            </CardActions>
+          </Card>
+
+          <Card>
+            <CardHeader title="Dan's Afternoon Commute" />
+            <CardContent>
+              <div>Prospect Park</div>
+              <div>West</div>
+              <div>10 min before</div>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained" size="small">Delete</Button>
+              <Button variant="contained" size="small">Edit</Button>
+              <Button variant="contained" size="small">Activate</Button>
+            </CardActions>
+          </Card>
+
           <button
             onClick={this.logout}
           >
