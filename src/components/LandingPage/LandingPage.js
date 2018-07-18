550984418d3537019d2688e022a9d5ebf25ecc9c
Dan Ross <dr.ross15@gmail.com>
2018-07-17 22:13:14 -0500

    dynamic quick alerts

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import Button from '@material-ui/core/Button';
 
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 
 class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            route: 901,
+            direction: 1,
+            stop: 56334,
+            when_to_alert: '',
+            routeList: []
+        }
+    }
     handleLoginButton = event => {
         this.props.history.push('/login');
     }
@@ -12,9 +24,63 @@ class LandingPage extends Component {
     handleRegisterButton = event => {
         this.props.history.push('/register');
     }
-    
+
+    handleInputChangeFor = propertyName => event => {
+        this.setState({
+          [propertyName]: event.target.value,
+        });
+        if(propertyName === 'route') {
+            console.log('in route change');
+            axios.get(`/api/alert/route/${event.target.value}`)
+                .then(response => {
+                    console.log(response.data);
+                    this.setState({
+                        ...this.state,
+                        routeList: response.data
+                    });
+                }).catch(err => {
+                    console.log({err});
+                    
+                })
+        }
+    }
 
     render() {
+        let routeList = null;
+        let directionList = null;
+
+        if(this.state.routeList.length === 0) {
+            routeList = (
+                    <select name="stop">
+                    </select>
+            )
+        } else {
+            routeList = (
+                    <select onChange={this.handleInputChangeFor('stop')} name="stop">
+                        {this.state.routeList.map((stop, i) => {
+                            return (
+                                <option key={i} value={stop.identifier}>{stop.name}</option>
+                            )
+                        })}
+                    </select>
+            )
+        }
+
+        if(this.state.route === '902') {
+            directionList = (
+                <select onChange={this.handleInputChangeFor('direction')} name="direction">
+                    <option value="2">East</option>
+                    <option value="3">West</option>
+                </select>
+            )
+        } else {
+            directionList = (
+                <select onChange={this.handleInputChangeFor('direction')} name="direction">
+                    <option value="1">South</option>
+                    <option value="4">North</option> 
+                </select>
+            )
+        }
         return(
             <div>
                  <Button variant="contained" onClick={() => this.handleLoginButton('/login')}>
@@ -31,7 +97,7 @@ class LandingPage extends Component {
                     <div>
                         <label htmlFor="route">
                         Route:
-                        <select name="route">
+                        <select name="route" onChange={this.handleInputChangeFor('route')}>
                             <option value="901">Blue Line</option>
                             <option value="902">Green Line</option>
                             <option value="903">Red Line</option>
@@ -41,19 +107,13 @@ class LandingPage extends Component {
                     <div>
                         <label htmlFor="Direction" >
                         Direction:
-                        <select name="direction">
-                            <option value="1">South</option>
-                            <option value="2">East</option>
-                            <option value="3">West</option>
-                            <option value="4">North</option>
-                        </select>
+                        { directionList }
                         </label>
                     </div>
                     <div>
                         <label htmlFor="Stop">
                         Stop:
-                        <select name="stop">
-                        </select>
+                        { routeList }
                         </label>
                     </div>
                     <div>
