3d7f01b2586e712f6bc8a939137e76e467345605
Dan Ross <dr.ross15@gmail.com>
2018-07-19 09:46:42 -0500

    getting the difference between when_to_alert and timeOfArrival

--- server/modules/alerts.js
+++ server/modules/alerts.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const axios = require('axios');
+const moment = require('moment');
 
 let activeAlerts = [];
 async function checkAlerts() {
     // SELECT ALL ACTIVE INPUTS
-    let queryText = `SELECT user_id, name, station_id, route_id FROM alerts 
+    let queryText = `SELECT user_id, name, station_id, route_id, direction, when_to_alert FROM alerts 
         JOIN stops ON alerts.stop_id = stops.id
         WHERE active=true;`;
     pool.query(queryText)
@@ -17,11 +18,30 @@ async function checkAlerts() {
         })
 
     await new Promise(resolve => {setTimeout(resolve, 1000)})
-
+    
+    let apiURL = '';
     // FOR ALL ACTIVE INPUTS
     for(alert of activeAlerts) {
-        let apiURL = `http://svc.metrotransit.org/NexTrip/${alert.route_id}/${alert.direction}/${alert.stop}`;
-        axios.get(apiURL)
+        apiURL = `http://svc.metrotransit.org/NexTrip/${alert.route_id}/${alert.direction}/${alert.station_id}?format=json`;
+        axios.get(apiURL).then(response => {
+            console.log(response.data[0]);
+            let timeOfArrival = moment(response.data[0].DepartureTime);
+            console.log({timeOfArrival});
+            let currentTime = moment();
+            console.log({currentTime});
+            
+            console.log(alert.when_to_alert);
+            
+            let timeDiff = moment(timeOfArrival.diff(currentTime)).format('m');
+            console.log({timeDiff});
+            if(alert.when_to_alert <= timeDiff) {
+                
+            }
+            
+        }).catch(err => {
+            console.log({err});
+            
+        });
         
     }
     // GET THE TIME_TO_NEXT_DEPARTURE
