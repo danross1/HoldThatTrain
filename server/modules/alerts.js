5d745dbb3e1171e9e52852c35385439cc6277726
Dan Ross <dr.ross15@gmail.com>
2018-07-20 23:10:33 -0500

    refactor alerts.js

--- server/modules/alerts.js
+++ server/modules/alerts.js
@@ -9,8 +9,9 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilio = require('twilio')(accountSid, authToken);
 
 let activeAlerts = [];
-async function checkAlerts() {    
-    // SELECT ALL ACTIVE INPUTS
+let apiURL = '';
+
+function selectAllActiveAlerts() {
     let queryText = `SELECT alerts.id, name, station_id, route_id, direction, when_to_alert, phone FROM alerts 
         JOIN stops ON alerts.stop_id = stops.id
         JOIN person ON alerts.user_id = person.id
@@ -22,47 +23,66 @@ async function checkAlerts() {
         }).catch(err => {
             console.log({err});
         })
+}
 
-    await new Promise(resolve => {setTimeout(resolve, 1000)})
-    
-    let apiURL = '';
+function deactivateAlert(alert_id) {
+    let queryText = `UPDATE alerts SET active=false WHERE id=$1`;
+        pool.query(queryText, [alert_id])
+            .then(response => {
+                console.log({response});
+            }).catch(err => {
+                console.log({err});
+            });
+}
+
+function sendTwilioMessage(alert) {
+    twilio.messages
+        .create({
+            body: `Your train at ${alert.station_id} is arriving soon!  Go catch that train!`,
+            from: '+12399709412',
+            to: alert.phone
+        })
+        .then(message => console.log(message.sid))
+        .done();
+}
+
+function checkTimeDifference(response, alert) {
+    console.log(response.data[0]);
+        let timeOfArrival = moment(response.data[0].DepartureTime);
+        console.log({timeOfArrival});
+        let currentTime = moment();
+        console.log({currentTime});
+        
+        console.log(alert.when_to_alert);
+        
+        let timeDiff = moment(timeOfArrival.diff(currentTime)).format('m');
+        console.log({timeDiff});
+        if(timeDiff <= alert.when_to_alert) {
+            sendTwilioMessage(alert);
+            deactivateAlert(alert.id);
+        }
+}
+
+function loopThroughActiveAlerts() {
     for(alert of activeAlerts) {
         apiURL = `http://svc.metrotransit.org/NexTrip/${alert.route_id}/${alert.direction}/${alert.station_id}?format=json`;
         axios.get(apiURL).then(response => {
-            console.log(response.data[0]);
-            let timeOfArrival = moment(response.data[0].DepartureTime);
-            console.log({timeOfArrival});
-            let currentTime = moment();
-            console.log({currentTime});
-            
-            console.log(alert.when_to_alert);
-            
-            let timeDiff = moment(timeOfArrival.diff(currentTime)).format('m');
-            console.log({timeDiff});
-            if(timeDiff <= alert.when_to_alert) {
-                twilio.messages
-                    .create({
-                        body: `Your train at ${alert.station_id} is arriving soon!  Go catch that train!`,
-                        from: '+12399709412',
-                        to: alert.phone
-                    })
-                    .then(message => console.log(message.sid))
-                    .done();
-                let queryText = `UPDATE alerts SET active=false WHERE id=$1`;
-                pool.query(queryText, [alert.id])
-                    .then(response => {
-                        console.log({response});
-                    }).catch(err => {
-                        console.log({err});
-                    });
-            }
-            
+            checkTimeDifference(response, alert);
         }).catch(err => {
             console.log({err});
             
         });
         
     }
+}
+
+async function checkAlerts() {    
+    selectAllActiveAlerts();
+
+    await new Promise(resolve => {setTimeout(resolve, 1000)})
+    
+    loopThroughActiveAlerts();
+    
 }
 
 checkAlerts();
